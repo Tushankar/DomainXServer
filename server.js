91f9f4788d36894611ca66dfc0e544507dfcbb21
TushankarKryptronix <tushankar1.kyptronix@outlook.com>
2025-10-07 18:54:56 +0530

    he

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ connectDB();
 // Middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "https://domainx.netlify.app",
+    origin: process.env.CLIENT_URL || "https://domainx.netlify.app" || "http://localhost:5173",
     credentials: true,
   })
 );
